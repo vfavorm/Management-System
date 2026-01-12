cba428950fcf949a8497fe8ccd290d13ffd3002f
Favor Vincent <vfavorm77@gmail.com>
2026-01-12 10:09:53 +0300

    Set up employee model

---
+++ CRUDapplication/app/Filament/Resources/Employees/Tables/EmployeesTable.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Filament\Resources\Employees\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Table;
+
+class EmployeesTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
